9b4702b78cbc1f682cb2c15ea58628d45aae7a59
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-09-05 10:57:08 +0700

    chore: update pnpm workspace configuration and add VSCode settings
    - Changed the pnpm workspace configuration to allow builds for '@swc/core'.
    - Added a new VSCode settings file with custom color themes for the workspace.

--- backend/db/client.ts
+++ backend/db/client.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
-import * as schema from "@db/schema.js";
+import * as schema from "./schema.js";
 
 export const dbConn = new Database("db.sqlite");
 export const dbClient = drizzle(dbConn, { schema: schema, logger: false });
--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -1,15 +1,29 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { dbClient } from "@db/client.js"; // your drizzle instance
-import { github as gh } from "@utils/env.js";
+import { dbClient } from "../../db/client.js"; // your drizzle instance
+import { github as gh } from "../utils/env.js";
 
 export const auth = betterAuth({
   database: drizzleAdapter(dbClient, {
     provider: "sqlite",
   }),
+  // 2026-09: Need to add account linking so that the user with existing emails and login with OAuth
+  account: {
+    accountLinking: {
+      enabled: true,
+      trustedProviders: ["github"],
+    },
+  },
   emailAndPassword: {
     enabled: true,
     minPasswordLength: 4,
+  },
+  // 2026-09: Email without verification will block account link, so I need to click at the link before using OAuth
+  emailVerification: {
+    sendOnSignUp: true,
+    sendVerificationEmail: async ({ user, url }) => {
+      console.log(`[Dev Verification URL] for ${user.email}: ${url}`);
+    },
   },
   socialProviders: {
     github: {
